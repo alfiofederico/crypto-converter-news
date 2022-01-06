2d7bf7ed8c4f9fefe81a85057f02a4ab9f9c9c9e
Alfio Federico Di Pinto <fefferico81@gmail.com>
2022-01-06 14:08:48 +0900

    converter done

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -1,16 +1,56 @@
 import ExchangeRate from "./ExchangeRate";
 import {useState} from 'react'
-
+import axios from "axios";
 
 function CurrencyConverter() {
 
   const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA']
   const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC')
-  const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('BTC')
+  const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('USD')
   const [amount, setAmount] = useState(1)
+  const [exchangeRate, setExchangeRate] = useState(0)
+  const [result, setResult] = useState(0)
 
-  console.log(chosenPrimaryCurrency)
+  const convert = (e) => {
+    e.preventDefault();
+    
+    const options = {
+      method: "GET",
+      url: "https://alpha-vantage.p.rapidapi.com/query",
+      params: {
+        from_currency: chosenPrimaryCurrency,
+        function: "CURRENCY_EXCHANGE_RATE",
+        to_currency: chosenSecondaryCurrency,
+      },
+      headers: {
+        "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+        "x-rapidapi-key": "9ce7885d3bmshfcfb9485dba8f27p18dd02jsn68d5f6e30fd8",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(
+          response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+        );
+        setExchangeRate(
+          response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+        );
+        setResult(
+          response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+        * amount)
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  console.log(ExchangeRate)
+
+/*   console.log(chosenPrimaryCurrency)
   console.log(chosenSecondaryCurrency)
+  console.log(amount, convert) */
   return (
     <div>
       <h3 className="font-bold text-center py-7">Currency Converter</h3>
@@ -72,31 +112,33 @@ function CurrencyConverter() {
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
               htmlFor="amount"
             >
-              Amount:
+              Converted Amount:
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               name="currency-amount-2"
               type="number"
-              value={""}
+              value={result}
+              disabled={true}
             />
           </div>
         </div>
 
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3 mb-6 md:mb-0">
-            <input
-              type="submit"
-              value="Convert"
+            <button
+              onClick={convert}
               className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            />
+            >
+              Convert
+            </button>
           </div>
         </div>
 
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="result w-full px-3 mb-6 md:mb-0">
             <h2 className="text-center text-5xl">
-              <ExchangeRate />
+              <ExchangeRate exchangeRate={exchangeRate} />
             </h2>
           </div>
         </div>
--- src/Components/ExchangeRate.js
+++ src/Components/ExchangeRate.js
@@ -1,9 +1,9 @@
-function ExchangeRate() {
+function ExchangeRate({exchangeRate}) {
   return (
-    <div>
-      Exchange Rate
+    <div className="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2">
+      <h3>Exchange Rate:</h3> {exchangeRate}
     </div>
-  )
+  );
 }
 
 export default ExchangeRate
